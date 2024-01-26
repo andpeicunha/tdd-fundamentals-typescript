08532d7a9b116923b40f8eaacacb3121eb5074c2
Peixoto <peixoto@trin.ca>
2024-01-26 08:51:25 -0300

    config first test

---
+++ first.test.ts
@@ -0,0 +1,9 @@
+describe('first', () => {
+    test('should return 1', () => {
+        expect(1).toBe(1)
+    })
+    it('should return 2', () => {
+        expect(2).toBe(2)
+    })
+})
+
---
+++ greeter.test.ts
@@ -0,0 +1,5 @@
+describe('testing greeter', () => {
+    it('', () => {
+        
+    })
+})
